1256da6e7c76ec6db4ef74d0bbeb100cc5defdfb
square <cyrushcuenca@gmail.com>
2019-04-05 16:30:14 -0700

    Working Alexa index scraping via webscraping to avoid AWIS charges

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,31 @@ import { Router } from 'express';
 import facets from './facets';
 const validUrl = require('valid-url');
 const url = require("url");
+const jsdom = require("jsdom");
+const request = require('request');
 
+function rate(url, rating) {
+	let link = 'https://www.alexa.com/siteinfo/' + url.host;
+	request(link, function (error, response, body) {
+	  	console.log('error:', error); // Print the error if one occurred
+		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+		  
+	  	const dom = new jsdom.JSDOM(body);
+		const alexaRank = parseInt(dom.window.document.getElementsByClassName('globleRank')[0].getElementsByClassName('metrics-data')[0].textContent);
+
+		// If alexaMultiplier is 1, it will have no effect on the rating
+		let alexaMultiplier = 1;
+
+		// If the site is ranked, create a multiplier larger than 1 to manipulate the rating
+		if(!isNaN(alexaRank)) {
+			alexaMultiplier = alexaRank; // Modify this to create a multiplier
+		}
+
+		// Do stuff with alexa
+
+		rating(alexaMultiplier);
+	});
+}
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -18,11 +42,15 @@ export default ({ config, db }) => {
 
 	// perhaps expose some API metadata at the root
 	api.get('/news/rate', (req, res) => {
-		var result = url.parse(req.query.link);
-		console.log('Rating ' + req.query.link);
+		// Validate URL
 		if (validUrl.isUri(req.query.link)){
 			console.log('Looks like an URI');
-			res.json({ confidence: 1, bias: "Far right"  });
+
+			// Rate story
+			rate(url.parse(req.query.link), (rating) => {
+				// Return ratings
+				res.json({ confidence: rating, bias: rating });
+			});
 		} 
 		else {
 			console.log('Not a URI');
